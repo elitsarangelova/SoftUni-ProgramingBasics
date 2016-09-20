13e3968d63cb908770e9955ea646cf1b22534f0e
elitsarangelova <elitsa.rangelova95@gmail.com>
2016-09-20 19:27:42 +0300

    loops

---
+++ loopsExcersices/videoTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace videoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+            for (int i = 1; i <= num; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                sum = sum + n;
+                if (n > max)
+                {
+                    max = n;
+                }
+               
+            }
+            if (max == sum - max)
+            {
+                Console.WriteLine("Yes Sum = {0}", max);
+            }
+            else
+            {
+                int diff = Math.Abs(max - (sum-max));
+                Console.WriteLine("No Diff = {0}", diff);
+            }
+        }
+    }
+}
